0f2325a45081ae927cf11a396c2d35222beb3477
Sinazo-078 <sihentshesinazo44@gmail.com>
2024-01-23 09:39:26 +0200

    first commit

---
+++ MANAGE/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+import Login from './Components/Login';
+
+function App() {
+  return (
+    <>
+      <Login/>
+    </>
+  );
+}
+
+export default App;
---
+++ MANAGE/src/Components/Login.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+// import 'bootstrap/dist/cs/bootsrap.min.css'
+
+const Login = () => {
+  return (
+    
+    <div className="d-flex justfy-content-center align-item-center vh-100 loginPage">
+    <div className="p-3 rounded w-25 border loginForm" >
+      <h2>Login Page</h2>
+      <form>
+   
+       <div>
+        <label htmlfor="email"><strong>Password</strong></label>
+        <input type="Email" name="email"autoComplete="off" placeholder="Enter Email"
+        className='form-control rounded-0'/>
+           </div> 
+           
+       <div>
+        <label htmlfor="password">Email:</label>
+        <input type="password" name="password"autoComplete="off" placeholder="Enter Email"
+        className='form-control rounded-0'/>
+
+           </div> 
+           <button className='btn btn sucess w-100 rounded-0'>Submit</button>
+        </form>
+    </div>  
+</div>
+  )
+}
+
+export default Login
