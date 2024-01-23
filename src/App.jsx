9bb4f60847915e8c533ac211addfe46806c29c0b
sinazo sihentshe <sihentshesinazo44@gmail.com>
2024-01-23 13:21:45 +0200

    initial files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+
+import Login from './Components/Login';
+
+function App() {
+  return (
+    <>
+      < Login />
+    </>
+  );
+}
+
+export default App;
