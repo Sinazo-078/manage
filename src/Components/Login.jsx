722081d66fdada1a5e99aeadedf52ea3d2b2b5dd
sinazo sihentshe <sihentshesinazo44@gmail.com>
2024-01-26 11:13:46 +0200

    updated project

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,32 +1,73 @@
-import React from 'react'
-// import 'bootstrap/dist/cs/bootsrap.min.css'
+import React, { useState } from 'react';
 import './style.css';
+import axios from 'axios';
 
 const Login = () => {
+  const [values, setValues] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post('http://localhost:3000/auth/adminlogin', values)
+      .then((result) => console.log(result))
+      .catch((err) => console.log(err));
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setValues((prevValues) => ({ ...prevValues, [name]: value }));
+  };
+
   return (
-    
-    <div className="d-flex justfy-content-center align-item-center vh-100 loginPage" >  
-    <div className="p-3 rounded w-25 border loginForm" >
-      <h2>Login Page</h2> 
-      <form>
-   
-       <div>
-        <label htmlfor="email"><strong>Password</strong></label>
-        <input type="Email" name="email"autoComplete="off" placeholder="Enter Email"
-        className='form-control rounded-0'/>
-           </div> 
-           
-       <div>
-        <label htmlfor="password">Email:</label>
-        <input type="password" name="password"autoComplete="off" placeholder="Enter Email"
-        className='form-control rounded-0'/>
+    <div className="d-flex justify-content-center align-items-center vh-100 loginPage">
+      <div className="p-3 rounded w-25 border loginForm">
+        <h2>Login Page</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="email">
+              <strong>Email</strong>
+            </label>
+            <input
+              type="email"
+              name="email"
+              autoComplete="off"
+              placeholder="Enter Email"
+              onChange={handleChange}
+              className="form-control rounded-0"
+            />
+          </div>
 
-           </div> 
-           <button className='btn btn sucess w-100 rounded-0'>Submit</button> 
+          <div className="mb-3">
+            <label htmlFor="password">
+              <strong>Password</strong>
+            </label>
+            <input
+              type="password"
+              name="password"
+              autoComplete="off"
+              placeholder="Enter Password"
+              onChange={handleChange}
+              className="form-control rounded-0"
+            />
+          </div>
+
+          <button className="btn btn-success w-100 rounded-0 mb-2" type="submit">
+            Submit
+          </button>
+
+          <div className="mb-3">
+            <input type="checkbox" name="tick" id="tick" className="me-2" />
+            <label htmlFor="tick">
+              <strong>You Agree with Terms & conditions</strong>
+            </label>
+          </div>
         </form>
-    </div>  
-</div>
-  )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
